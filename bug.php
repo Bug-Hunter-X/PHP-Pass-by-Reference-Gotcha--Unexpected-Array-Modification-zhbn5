6d5c0b7041ee8575378544b13a2fe27d539478ae
BugHunter-X <askzenai@gmail.com>
2025-01-08 22:06:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,16 @@
+```php
+<?php
+function processData(array $data): array {
+  foreach ($data as &$value) {
+    if (is_string($value)) {
+      $value = strtolower($value);
+    }
+  }
+  return $data;
+}
+
+$myData = ['apple', 'Banana', 'Orange'];
+$processedData = processData($myData);
+print_r($processedData);
+?>
+```
